78f8d9c7dfc50e247a00ead636603649d2e22f9b
Ahmad Zuiod <armousa19@cit.just.edu.jo>
2023-01-24 13:36:54 +0300

    Commit

--- Task One/main.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-from pytesseract import pytesseract
-Tesseract_Path = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-ImagePath = input("Please enter image path :")
-
-pytesseract.tesseract_cmd = Tesseract_Path
-Image = Image.open(ImagePath)
-ImageText = pytesseract.image_to_string(Image)
-print(ImageText)
